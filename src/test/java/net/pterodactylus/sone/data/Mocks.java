d4b256f5b8d0f89795d6b86c0ae2b0d57904782c
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2014-02-19 21:50:50 +0100

    Fix import.

--- src/test/java/net/pterodactylus/sone/data/Mocks.java
+++ src/test/java/net/pterodactylus/sone/data/Mocks.java
@@ -25,7 +25,6 @@ import static com.google.common.collect.Maps.newHashMap;
 import static com.google.common.collect.Ordering.from;
 import static java.util.Collections.emptySet;
 import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyBoolean;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.doAnswer;
